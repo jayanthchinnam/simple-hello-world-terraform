f8b7a12bd6de4a43700fc30bf8a4fe170c26efb6
jayanthtechm <jayanthtechm@gmail.com>
2022-06-17 00:57:02 +0530

    variable sample code

--- output.tf
+++ output.tf
@@ -1,3 +1,14 @@
-output "abc" {
-  value= "hello-Jay_world"
+# output "abc" {
+#   value= "hello-Jay_world"
+# }
+
+
+
+variable "abc" {
+  default = "100"
 }
+
+output "abc" {
+  value = var.abc
+}
+
